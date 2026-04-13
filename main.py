ca81bc8eac79aee05438adb53a6b373655b2b676
David Burgos <dburgos1@stetson.edu>
2026-04-13 17:05:24 +0000

    Changed the date

---
+++ main.py
@@ -0,0 +1,3 @@
+# do changes -> git add filename (only if u use terminal) -> go to source control -> verify ur changes -> commit -> commit message -> sync changes / push
+
+# git clone reponame/ssh/http -> fetch from all remotes -> pull 
