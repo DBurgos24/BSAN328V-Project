063fbfde504522072cc73d43f1e8ed193db62ccd
David Burgos <dburgos1@stetson.edu>
2026-04-16 14:31:10 +0000

    Edits

--- main.py
+++
@@ -1,3 +0,0 @@
-# do changes -> git add filename (only if u use terminal) -> go to source control -> verify ur changes -> commit -> commit message -> sync changes / push
-
-# git clone reponame/ssh/http -> fetch from all remotes -> pull 
